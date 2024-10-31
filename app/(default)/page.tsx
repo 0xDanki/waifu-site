2db0bf17aa6d38d37eccca9bea4473ba6954c2a5
0xDanki <typepuller@gmail.com>
2024-11-01 00:04:35 +0800

    initial commit

---
+++ app/(default)/page.tsx
@@ -0,0 +1,22 @@
+export const metadata = {
+  title: "WAIFU Project - Web3 UI Pack and Tailwind CSS Components Library",
+  description: "Web3 UI Pack and Tailwind CSS Components Library",
+};
+
+import Hero from "@/components/hero-home";
+import BusinessCategories from "@/components/business-categories";
+import FeaturesPlanet from "@/components/features-planet";
+import LargeTestimonial from "@/components/large-testimonial";
+import Cta from "@/components/cta";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <BusinessCategories />
+      <FeaturesPlanet />
+      <LargeTestimonial />
+      <Cta />
+    </>
+  );
+}
---
+++ components/features-planet.tsx
@@ -0,0 +1,135 @@
+import Image from "next/image";
+import PlanetImg from "@/public/images/planet.png";
+import PlanetOverlayImg from "@/public/images/planet-overlay.svg";
+import PlanetTagImg01 from "@/public/images/planet-tag-01.png";
+import PlanetTagImg02 from "@/public/images/planet-tag-02.png";
+import PlanetTagImg03 from "@/public/images/planet-tag-03.png";
+import PlanetTagImg04 from "@/public/images/planet-tag-04.png";
+
+export default function FeaturesPlanet() {
+  return (
+    <section className="relative before:absolute before:inset-0 before:-z-20 before:bg-gray-900">
+      <div className="mx-auto max-w-6xl px-4 sm:px-6">
+        <div className="py-12 md:py-20">
+          {/* Section header */}
+          <div className="mx-auto max-w-3xl pb-16 text-center md:pb-20">
+            <h2 className="text-3xl font-bold text-gray-200 md:text-4xl">
+              WAIFU helps designers and developers iterate on dApps faster
+            </h2>
+          </div>
+          {/* Grid */}
+          <div className="grid overflow-hidden sm:grid-cols-2 lg:grid-cols-3 [&>*]:relative [&>*]:p-6 [&>*]:before:absolute [&>*]:before:bg-gray-800 [&>*]:before:[block-size:100vh] [&>*]:before:[inline-size:1px] [&>*]:before:[inset-block-start:0] [&>*]:before:[inset-inline-start:-1px] [&>*]:after:absolute [&>*]:after:bg-gray-800 [&>*]:after:[block-size:1px] [&>*]:after:[inline-size:100vw] [&>*]:after:[inset-block-start:-1px] [&>*]:after:[inset-inline-start:0] md:[&>*]:p-10">
+            <article>
+              <h3 className="mb-2 flex items-center space-x-2 font-medium text-gray-200">
+              <svg
+                  className="fill-blue-500"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width={16}
+                  height={16}
+                >
+                  <path d="M14.29 2.614a1 1 0 0 0-1.58-1.228L6.407 9.492l-3.199-3.2a1 1 0 1 0-1.414 1.415l4 4a1 1 0 0 0 1.496-.093l7-9ZM1 14a1 1 0 1 0 0 2h14a1 1 0 1 0 0-2H1Z" />
+                </svg>
+                <span>Figma UI Pack</span>
+              </h3>
+              <p className="text-[15px] text-gray-400">
+                Prototype quickly with ready-made complex web3 components on
+                Figma. WAIFU UI pack features well thought templates for DeFi, 
+                Wallets, AA, and NFTs based on UX design principles.
+              </p>
+            </article>
+            <article>
+              <h3 className="mb-2 flex items-center space-x-2 font-medium text-gray-200">
+                <svg
+                  className="fill-blue-500"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width={16}
+                  height={16}
+                >
+                  <path d="M14.29 2.614a1 1 0 0 0-1.58-1.228L6.407 9.492l-3.199-3.2a1 1 0 1 0-1.414 1.415l4 4a1 1 0 0 0 1.496-.093l7-9ZM1 14a1 1 0 1 0 0 2h14a1 1 0 1 0 0-2H1Z" />
+                </svg>
+                <span>Components Library</span>
+              </h3>
+              <p className="text-[15px] text-gray-400">
+                You'll never build from scratch again. Rapidly develop and iterate on web3 frontends with a  
+                Tailwind CSS components library that is made to simplify crypto
+                experiences.
+              </p>
+            </article>
+            <article>
+              <h3 className="mb-2 flex items-center space-x-2 font-medium text-gray-200">
+                <svg
+                  className="fill-blue-500"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width={16}
+                  height={16}
+                >
+                  <path d="M14.29 2.614a1 1 0 0 0-1.58-1.228L6.407 9.492l-3.199-3.2a1 1 0 1 0-1.414 1.415l4 4a1 1 0 0 0 1.496-.093l7-9ZM1 14a1 1 0 1 0 0 2h14a1 1 0 1 0 0-2H1Z" />
+                </svg>
+                <span>Web3 UX System</span>
+              </h3>
+              <p className="text-[15px] text-gray-400">
+                Interaction patterns across the industry should be uniform
+                in order to be habit-forming. By opensourcing both the tools and the guidelines, 
+                similar flows and expectations can be practiced by the users. 
+              </p>
+            </article>
+            <article>
+              <h3 className="mb-2 flex items-center space-x-2 font-medium text-gray-200">
+              <svg
+                  className="fill-blue-500"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width={16}
+                  height={16}
+                >
+                  <path d="M10.284.33a1 1 0 1 0-.574 1.917 6.049 6.049 0 0 1 2.417 1.395A1 1 0 0 0 13.5 2.188 8.034 8.034 0 0 0 10.284.33ZM6.288 2.248A1 1 0 0 0 5.718.33 8.036 8.036 0 0 0 2.5 2.187a1 1 0 0 0 1.372 1.455 6.036 6.036 0 0 1 2.415-1.395ZM1.42 5.401a1 1 0 0 1 .742 1.204 6.025 6.025 0 0 0 0 2.79 1 1 0 0 1-1.946.462 8.026 8.026 0 0 1 0-3.714A1 1 0 0 1 1.421 5.4Zm2.452 6.957A1 1 0 0 0 2.5 13.812a8.036 8.036 0 0 0 3.216 1.857 1 1 0 0 0 .574-1.916 6.044 6.044 0 0 1-2.417-1.395Zm9.668.04a1 1 0 0 1-.041 1.414 8.033 8.033 0 0 1-3.217 1.857 1 1 0 1 1-.571-1.917 6.035 6.035 0 0 0 2.415-1.395 1 1 0 0 1 1.414.042Zm2.242-6.255a1 1 0 1 0-1.946.462 6.03 6.03 0 0 1 0 2.79 1 1 0 1 0 1.946.462 8.022 8.022 0 0 0 0-3.714Z" />
+                </svg>
+                <span>Developers x Designers</span>
+              </h3>
+              <p className="text-[15px] text-gray-400">
+                Allows designers and developers to collaborate efficiently by using the same
+                UI pack and frontend library. WAIFU bridges communication gaps 
+                as well as empower nimble teams with good dApp design. 
+              </p>
+            </article>
+            <article>
+              <h3 className="mb-2 flex items-center space-x-2 font-medium text-gray-200">
+                <svg
+                  className="fill-blue-500"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width={16}
+                  height={16}
+                >
+                  <path d="M10.284.33a1 1 0 1 0-.574 1.917 6.049 6.049 0 0 1 2.417 1.395A1 1 0 0 0 13.5 2.188 8.034 8.034 0 0 0 10.284.33ZM6.288 2.248A1 1 0 0 0 5.718.33 8.036 8.036 0 0 0 2.5 2.187a1 1 0 0 0 1.372 1.455 6.036 6.036 0 0 1 2.415-1.395ZM1.42 5.401a1 1 0 0 1 .742 1.204 6.025 6.025 0 0 0 0 2.79 1 1 0 0 1-1.946.462 8.026 8.026 0 0 1 0-3.714A1 1 0 0 1 1.421 5.4Zm2.452 6.957A1 1 0 0 0 2.5 13.812a8.036 8.036 0 0 0 3.216 1.857 1 1 0 0 0 .574-1.916 6.044 6.044 0 0 1-2.417-1.395Zm9.668.04a1 1 0 0 1-.041 1.414 8.033 8.033 0 0 1-3.217 1.857 1 1 0 1 1-.571-1.917 6.035 6.035 0 0 0 2.415-1.395 1 1 0 0 1 1.414.042Zm2.242-6.255a1 1 0 1 0-1.946.462 6.03 6.03 0 0 1 0 2.79 1 1 0 1 0 1.946.462 8.022 8.022 0 0 0 0-3.714Z" />
+                </svg>
+                <span>Freedom to Build</span>
+              </h3>
+              <p className="text-[15px] text-gray-400">
+                Solo hacker? No problem. WAIFU provides you the basic blocks
+                of crypto frontend so you can focus on what you do best-- working
+                on the main logic of your application.
+              </p>
+            </article>
+            <article>
+              <h3 className="mb-2 flex items-center space-x-2 font-medium text-gray-200">
+              <svg
+                  className="fill-blue-500"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width={16}
+                  height={16}
+                >
+                  <path d="M10.284.33a1 1 0 1 0-.574 1.917 6.049 6.049 0 0 1 2.417 1.395A1 1 0 0 0 13.5 2.188 8.034 8.034 0 0 0 10.284.33ZM6.288 2.248A1 1 0 0 0 5.718.33 8.036 8.036 0 0 0 2.5 2.187a1 1 0 0 0 1.372 1.455 6.036 6.036 0 0 1 2.415-1.395ZM1.42 5.401a1 1 0 0 1 .742 1.204 6.025 6.025 0 0 0 0 2.79 1 1 0 0 1-1.946.462 8.026 8.026 0 0 1 0-3.714A1 1 0 0 1 1.421 5.4Zm2.452 6.957A1 1 0 0 0 2.5 13.812a8.036 8.036 0 0 0 3.216 1.857 1 1 0 0 0 .574-1.916 6.044 6.044 0 0 1-2.417-1.395Zm9.668.04a1 1 0 0 1-.041 1.414 8.033 8.033 0 0 1-3.217 1.857 1 1 0 1 1-.571-1.917 6.035 6.035 0 0 0 2.415-1.395 1 1 0 0 1 1.414.042Zm2.242-6.255a1 1 0 1 0-1.946.462 6.03 6.03 0 0 1 0 2.79 1 1 0 1 0 1.946.462 8.022 8.022 0 0 0 0-3.714Z" />
+                </svg>
+                <span>Community</span>
+              </h3>
+              <p className="text-[15px] text-gray-400">
+                Have a concrete point of UI/UX discussions in Web3. The component 
+                designs and layout can change according to what the community decides.
+                The project is open to feedback and new contributors. 
+              </p>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
